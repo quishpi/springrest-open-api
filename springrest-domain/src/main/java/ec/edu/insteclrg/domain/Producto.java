76e745b25e5fdd58845c4ad59cbd2ac77f04dc27
Pedro Ordoñez <pedroo382@gmail.com>
2022-06-13 17:23:03 -0500

    Product Create

---
+++ springrest-domain/src/main/java/ec/edu/insteclrg/domain/Producto.java
@@ -0,0 +1,52 @@
+package ec.edu.insteclrg.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+public class Producto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private long id;
+	
+	@Column (unique = false, nullable = false)
+	private String codigo;
+	
+	@Column (nullable = false)
+	private String name;
+	
+
+	@Column (nullable = false)
+	private String descripcion;
+	
+
+	@Column (nullable = false)
+	private int precioventa;
+
+
+	@Column (nullable = false)
+	private int stockmin;
+	
+
+	@Column (nullable = false)
+	private int stockmax;
+	
+
+	/*@Column (updatable = false, unique = true, nullable = false)
+	private long idimpuesto;
+	
+	@Column (updatable = false, unique = true, nullable = false)
+	private long idtarifaiva;
+	
+	@Column (updatable = false, unique = true, nullable = false)
+	private long idtarifaice;*/
+}
